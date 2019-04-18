b84db121c4e1156fcd322af69f723524a1e8c0f1
Nathe <nathanquilley@hotmail.com>
2019-04-18 13:48:06 +0200

    uploading files

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <app-header :title="title" @changeTitle="updateTitle($event)"></app-header>
+    <app-ninjas :ninjas="ninjas"></app-ninjas>   
+    <app-footer :title="title"></app-footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Ninjas from './components/Ninjas.vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    'app-ninjas':Ninjas,
+    'app-header':Header,
+    'app-footer':Footer
+  },
+  data(){
+    return{
+      ninjas: [
+                {name: 'Ryu', speciality: 'Vue Components', show: false},
+                {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
+                {name: 'Hitoshi', speciality: 'Click Events', show: false},
+                {name: 'Tango', speciality: 'Conditionals', show: false},
+                {name: 'Kami', speciality: 'Webpack', show: false},
+                {name: 'Yoshi', speciality: 'Data Diggin', show: false}
+          ],
+      title: 'Vue Ninjas'
+    }
+  },
+  methods: {
+    updateTitle: function(updatedTitle){
+      this.title = updatedTitle
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
